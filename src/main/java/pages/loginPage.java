a799a2f6d62efebb3782a4979fbd555d618b5228
Rishabh Verma <rishab4verma@gmail.com>
2021-06-05 13:41:39 +0530

    changes based on feedback

--- src/main/java/pages/loginPage.java
+++ src/main/java/pages/loginPage.java
@@ -10,10 +10,10 @@ public class loginPage {
 	
 	public void test2()
 	{
-		System.out.println("login2");
+		System.out.println("login22");
 	}
 	
-	public void test3()
+	public void test3(String str)
 	{
 		System.out.println("login3");
 	}
