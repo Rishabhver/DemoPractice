6258542adb67dc2c5248af0a2c070aafc445eeed
Rishabh Verma <42169402+Rishabhver@users.noreply.github.com>
2021-06-05 16:43:03 +0530

    Update HomePage.java

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -19,6 +19,8 @@ public class HomePage {
 		System.out.println("This code is now in new branch3");
 		
 		System.out.println("This code is now in new branch4");
+		
+		System.out.println("Home Page6");
 
 	}
 
